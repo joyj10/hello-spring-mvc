f12d027cf3c3376975b42bfdea9139524e998f32
lia.jung <lia.jung@kakaocorp.com>
2023-11-04 21:25:17 +0900

    # HTTP 요청 파라미터 - @ModelAttribute

---
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,17 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+
+/**
+ * HelloData
+ * <pre>
+ * Describe here
+ * </pre>
+ *
+ * @version 1.0,
+ */
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
